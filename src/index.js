dfc6265cda9be6af0732531b6cbd8b72133d3740
nishant-tm <nishant.jain@teramatrix.co>
2019-03-23 23:11:27 +0530

    exact prop added with login route.

--- src/index.js
+++ src/index.js
@@ -3,61 +3,34 @@
  **/
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-	Route,
-	Switch,
-	BrowserRouter as Router
-} from 'react-router-dom'
+import { Route, Switch , BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import PageNotFound from './components/PageNotFound';
-import {
-	Provider
-} from 'react-redux';
+import { Provider } from 'react-redux';
 import PrivateRoute from './components/PrivateRoute';
 import store from './store';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( <
-	Provider store = {
-		store
-	} >
-	<
-	Router >
-
-	<
-	Switch >
-	<
-	Route exact path = "/"
-	component = {
-		App
-	}
-	/> <
-	Route path = "/login"
-	component = {
-		Login
-	}
-	/> <
-	PrivateRoute exact path = "/dashboard"
-	component = {
-		Dashboard
-	}
-	/> <
-	Route component = {
-		PageNotFound
-	}
-	/> <
-	/Switch>
-
-
-	<
-	/Router>
-
-	<
-	/Provider>,
-	document.getElementById('root'));
+ReactDOM.render( 
+	<Provider store={store}>
+		<Router>
+		     
+		    <Switch>
+		      <Route exact path="/" component={App} />
+		      <Route exact path="/login" component={Login} />
+		      <PrivateRoute exact path="/dashboard" component={Dashboard} />
+		      <Route path="*" component={PageNotFound} />
+		       
+		</Switch>
+		      
+		    
+  		</Router>
+	  
+	 </Provider>,
+	 document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
